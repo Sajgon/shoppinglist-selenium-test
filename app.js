6b4fb360af4cacf54c5fe3051cb70981532d56c7
Anton Johansson <anton.xdd@gmail.com>
2017-09-11 14:47:34 +0200

    more manual test examples & global random category variable

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ let GroceryListTest = require('./grocery-list-item');
 
 
 // Tillåtna kategorier
-let categories = ["Frukt", "Grönsak", "Kött", "Mejeriprodukt", "Krydda", "Övrigt"];
-
+var itemCategories = ["Frukt", "Grönsak", "Kött", "Mejeriprodukt", "Krydda", "Övrigt"];
+// En random tillåten kategori
+var randomItemCategory = itemCategories[Math.floor(itemCategories.length*Math.random())]; 
 
 // Make own tests
-
 let a = new GroceryList('Första Listan');
 a.addItem('Nötfärs', 'Matvaror att köpa', 2);
+//a.addItem('Päron', 'Frukt', 4);
+//a.addItem('Is', randomItemCategory, 5);
 
 console.log(a);
