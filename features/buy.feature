a180a360078c41fab47e5c91dd6cf545a88dc54b
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2017-09-12 12:03:45 +0200

    Buy test. Feature and test in need of some reviewing.

--- features/buy.feature
+++ features/buy.feature
@@ -4,25 +4,24 @@ Feature: Mark an item as bought
 
     Scenario Outline: Mark an item as bought
         Given that I have a grocery list
-        And added <number> of the item <item>
-        And with the category <category>
+        And I added <number> of the item <item>
+        And with category <category>
         When I buy the item <item>
         Then the item <item> should be marked as bought
 
         Examples:
-            |items   |category  |number|
+            |item    |category  |number|
             |milk    |dairy     |5     |
             |cucumber|vegetables|3     |
             |banana  |     fruit|6     |
 
     Scenario Outline: Trying to mark an non-existent item as bought
         Given that I have a grocery list
-        When I try to mark the (non-existent) item <item>
+        When I try to mark the (non-existent) item <item> as bought
         Then a warning will be displayed
-        And nothing will happen
 
         Examples:
-            |items   |
+            |item   |
             |milk    |
             |cucumber|
             |banana  |
---
+++ features/step_definitions/buy.js
@@ -0,0 +1,53 @@
+let assert = require('assert');
+let {defineSupportCode} = require('cucumber');
+let GroceryList = require('../../grocery-list.js');
+
+defineSupportCode(function({Given, When, Then}) {
+
+    let TestList;
+    let name = "";
+    let temp;
+
+    Given('that I have a grocery list', function() {
+        name = "My Grocery List"
+        TestList = new GroceryList(name);
+        assert.ok(TestList instanceof GroceryList);
+    });
+    
+    Given(/I added (.*) of the item (.*)/, function(quantity,item) {
+       temp = [quantity,item];
+
+    });
+
+    Given(/with category (.*)/, function(category) {
+        let [quantity, item] = temp;
+        assert.doesNotThrow(()=>TestList.addItem(item,quantity,category));
+    });
+    
+    When(/I buy the item (.*)/, function(item) {
+        assert.doesNotThrow(()=>TestList.buy(item));
+    });
+
+    Then(/the item (.*) should be marked as bought/, function (item) {
+        for(let i=0; i < TestList.items.length; i++){
+            if(TestList.items[i].name == item){
+                assert(TestList.items[i].bought == true);
+            }
+        }
+    });
+
+    //NON_EXISTENT ITEMS
+
+    When(/I try to mark the \(non-existent\) item (.*) as bought/, function(item) {
+        //TestList.buy(item);
+    });
+    
+    Then('a warning will be displayed', function() {
+        assert.throws(
+            () => {
+                TestList.buy(item);
+            },
+            Error
+        );
+    });
+});
