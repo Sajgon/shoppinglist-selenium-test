a02e6bc3e517669fe71a5e09eeb0d138806b6627
Ola Frick <info@culturehero.org>
2017-09-17 21:45:27 +0200

    Moved unnecessary testing code from app.js to the more suitably named playground.js

---
+++ playground.js
@@ -0,0 +1,28 @@
+process.on('unhandledRejection', r => console.log(r));
+
+// Import the GroceryListTest class
+let GroceryList = require('./grocery-list');
+let GroceryListItem = require('./grocery-list-item');
+
+
+// Tillåtna kategorier [Not in use]
+var itemCategories = ["Frukt", "Grönsak", "Kött", "Mejeriprodukt", "Krydda", "Övrigt"];
+// En random tillåten kategori
+var randomItemCategory = itemCategories[Math.floor(itemCategories.length*Math.random())];
+
+// Ola Test
+let a = new GroceryList('Första Listan');
+a.addItem('Beer', 1, 'Mat',);
+a.addItem('Plommon', 120, 'Mat');
+a.addItem('Väska', 120, 'Stuff');
+a.addItem('Lingon', 120, 'Mat');
+a.addItem('Abba', 1, 'Stuff');
+a.addItem('Saab', 1, 'Stuff');
+a.removeItem('Plommon');
+
+// a.sortAlphabetical();
+
+// console.log('***sorted', a.sortAlphabetical());
+// console.log('***NOT sorted', a.items);
+
+// process.on('unhandledRejection', r => console.log(r));
