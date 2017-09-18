c3d14af8ebbfaf000c4fd5eb5b87ad767bac16ff
Ola Frick <info@culturehero.org>
2017-09-18 14:25:17 +0200

    Added two simple view tests 'master-view' & 'detail-view' by jQuery.hide() in app-gui.js

--- app-gui.js
+++ app-gui.js
@@ -4,33 +4,34 @@ class AppGui {
     constructor(){
 
         //start with all views hidden
-        $('view').hide()
+
+
+        $('.master-view').hide()
+        $('.detail-view').hide()
         //show start view
-        $('start-view').show();
+        // $('master-view').show();
         this.defineEventListeners();
     }
 
-    //put all event listeners here
     defineEventListeners(){
-        //add list
         $(document).on('click','#addList',function(){
             //click it
         });
 
-        //add item
         $(document).on('click','#addItem',function(){
             console.log(this)
         });
 
-        //buy item
         $(document).on('click', '#buyItem', function(){
 
         })
 
-        //remove item
         $(document).on('click', '#removeItem', function(){
 
         })
     }
+
+
 }
-new AppGui();
+
+$(()=>new AppGui());
