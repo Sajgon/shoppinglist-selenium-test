86709aad3dbc8a0f29b715e20969d5588c823381
Ola Frick <info@culturehero.org>
2017-09-18 15:34:40 +0200

    Added AddItem bootstrap input form & most functionality

--- app-gui.js
+++ app-gui.js
@@ -1,37 +1,52 @@
 let GroceryList = require('./grocery-list');
 
 class AppGui {
-    constructor(){
 
-        //start with all views hidden
+  constructor() {
+    //start with all views hidden
 
+    $('.master-view').hide();
+    $('.detail-view').show();
 
-        $('.master-view').show()
-        $('.detail-view').hide()
-        //show start view
-        // $('master-view').show();
-        this.defineEventListeners();
-    }
+    this.defineEventListeners();
+    this.addItemForm();
+  }
 
-    defineEventListeners(){
-        $(document).on('click','#addList',function(){
-            //click it
-        });
+  defineEventListeners() {
+    $(document).on('click', '#addList', function() {
+      //click it
+    });
 
-        $(document).on('click','#addItem',function(){
-            console.log(this)
-        });
+    $(document).on('click', '#addItem', function() {
+      // console.log(this)
+    });
 
-        $(document).on('click', '#buyItem', function(){
+    $(document).on('click', '#buyItem', function() {
 
-        })
+    })
 
-        $(document).on('click', '#removeItem', function(){
+    $(document).on('click', '#removeItem', function() {
 
-        })
-    }
+    })
 
+  }
 
+  addItemForm() {
+
+    $('#new-item-form').submit(function(e) {
+
+      let name = $('.item-form-name').val();
+      let qty = $('.item-form-qty').val();
+      let category = $('.item-form-category').val();
+
+      // TODO: find out a way to add item to THIS list.
+      // ??? new GroceryListItem(name, qty, category);
+
+      console.log(name, qty, category);
+      $('#new-item-form').clear();
+      e.preventDefault();
+    });
+  }
 }
 
-$(()=>new AppGui());
+$(() => new AppGui());
