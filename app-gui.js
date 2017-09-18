a8b8202e032116a2bc3bae2d03942121f32c3ab3
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2017-09-18 14:41:30 +0200

    some app-gut

--- app-gui.js
+++ app-gui.js
@@ -8,29 +8,51 @@ class AppGui {
         //show start view
         $('start-view').show();
         this.defineEventListeners();
+        this.printList(myGroceryList.items);
     }
 
     //put all event listeners here
     defineEventListeners(){
+
+        let that = this;
+
         //add list
         $(document).on('click','#addList',function(){
             //click it
         });
 
-        //add item
-        $(document).on('click','#addItem',function(){
-            console.log(this)
-        });
-
         //buy item
         $(document).on('click', '#buyItem', function(){
 
-        })
+        });
+
+        //add item
+        $(document).on('click','#addItem',function(){
+            myGroceryList.addItem("Hola", 3, "kalas");
+            that.printList(myGroceryList.items);
+        });
 
         //remove item
-        $(document).on('click', '#removeItem', function(){
 
-        })
+        $(document).on('click', '#removeItem', function(){
+            that.printList(myGroceryList.items);
+
+        });
     }
+
+    printList(arr){
+        $('.list').empty();
+        arr.forEach(function(item){
+            $('.list').append('<li>' + item.name + "</li>")
+        })
+    };
 }
+
+let myGroceryList = new GroceryList("My Grocery List")
+myGroceryList.addItem("Fuktboll", 2, "övrigt");
+myGroceryList.addItem("Tomat", 3, "grönsak");
+myGroceryList.addItem("Äpple", 2, "frukt");
+myGroceryList.addItem("bacon", 2, "kött");
+myGroceryList.addItem("korv", 2, "kött");
+
 new AppGui();
